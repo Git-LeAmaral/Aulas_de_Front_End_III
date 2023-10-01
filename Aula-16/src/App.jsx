a324bee0b58e95a01a21cb1fb1f9f84dc91a14be
Leandro Amaral <lramaral02m@gmail.com>
2023-10-01 11:46:03 -0300

    Praticando com useCallback, Axios

--- Aula-16/src/App.jsx
+++ Aula-16/src/App.jsx
@@ -1,46 +1,5 @@
-import { useEffect, useState } from 'react'
-import { PerfilGithub } from './components/PerfilGithub'
-import styles from './app.module.css'
+import { RouteList } from './routes'
 
-function App() {
-  const [perfil, setPerfil] = useState({})
-  const [exibePerfil, setExibePerfil] = useState(false)
-
-  useEffect(() => {
-    async function carregarPerfil() {
-      try {
-        const response = await fetch(
-          'https://api.github.com/users/Git-LeAmaral'
-        )
-        const perfil = await response.json()
-        setPerfil(perfil)
-      } catch (error) {
-        console.log(error.message)
-      }
-    }
-
-    carregarPerfil()
-  }, [])
-
-  const alternaVisibilidade = () => {
-    setExibePerfil(valorAnterior => !valorAnterior)
-  }
-
-  return (
-    <>
-      <main className={styles.main}>
-        <section id="card" className={styles.cardSection}>
-          <button onClick={alternaVisibilidade}>
-            {exibePerfil ? 'Esconder' : 'Exibir'} Perfil{' '}
-          </button>
-        </section>
-
-        <div className={styles.cardContainer}>
-          {exibePerfil && <PerfilGithub perfil={perfil} />}
-        </div>
-      </main>
-    </>
-  )
+export function App() {
+  return <RouteList />
 }
-
-export default App
