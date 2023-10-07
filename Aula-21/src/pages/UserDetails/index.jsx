77f8d612f4ada360e64a7ddc2b1e15db155d0239
Leandro Amaral <lramaral02m@gmail.com>
2023-10-07 20:27:20 -0300

    Custom Hooks

--- Aula-21/src/pages/UserDetails/index.jsx
+++ Aula-21/src/pages/UserDetails/index.jsx
@@ -1,23 +1,33 @@
 import { useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useApiRequest} from '../../hooks/useApiRequest'
 import { GithubContext } from '../../contexts/GithubContext'
 import { PerfilGithub } from '../../components/PerfilGithub'
 
 import styles from './userDetails.module.css'
 
 export function UserDetails() {
-  const { repositoryOwner } = useParams()
+  const { repositoryOwner } = useParams();
+  const repositoriosPerfil = useApiRequest(`/users/${repositoryOwner}/repos`)
   const { listaDePerfis, buscarPerfilPeloLogin } = useContext(GithubContext)
 
-  const perfil = buscarPerfilPeloLogin(listaDePerfis, {
-    login: repositoryOwner
-  })
+  let perfil = buscarPerfilPeloLogin(listaDePerfis, repositoryOwner);
 
   return (
-    <section className={styles.UserDetails}>
+    <section className={styles.userDetails}>
       <div className={styles.cardContainer}>
         <PerfilGithub perfil={perfil} />
       </div>
+      <h1>Esses são os meus repositórios</h1>
+      <ul>
+        {repositoriosPerfil && repositoriosPerfil.map(repositorio => (
+          <li key={repositorio.id}>
+            <Link to={`/${repositoryOwner}/${repositorio.name}`}>
+              {repositorio.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
